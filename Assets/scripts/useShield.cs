099b8851e0d0b58d8543957e74d25573dd869faa
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-04-11 14:56:29 +0100

    I JUST MAKE IT EASY TA TA TA

--- Assets/scripts/useShield.cs
+++ Assets/scripts/useShield.cs
@@ -7,36 +7,9 @@ using UnityEngine.InputSystem.Interactions;
 public class useShield : MonoBehaviour
 {
     public GameObject shield;
-    public Rigidbody2D charRbody;
-
-
+   
     public bool shieldActive;
 
-    private void Awake()
-    {
-
-        charRbody = gameObject.GetComponentInParent<Rigidbody2D>();
-
-    }
-
-
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if (collision.CompareTag("rock"))
-    //    {
-    //        charRbody.gravityScale = 0;
-    //    }
-
-    //}
-
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if (collision.CompareTag("rock"))
-    //    {
-    //        charRbody.gravityScale = 1;
-    //    }
-
-    //}
     public void callShield(InputAction.CallbackContext context)
     {
         if (context.interaction is PressInteraction)
